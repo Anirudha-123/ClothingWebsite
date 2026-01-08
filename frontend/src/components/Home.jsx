3ab4ffea53df24af32580a3b5aeddbfed3a1f44a
Anirudha <anirudhakolekar65@gmail.com>
2026-01-08 19:08:08 +0530

    home comp update

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="container mx-auto px-4">
+      <div className="hero">
+        <img
+          src="about3.jpg"
+          alt="hero"
+          className="w-full h-64 sm:h-80 md:h-96 lg:h-125 object-cover rounded-md"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
