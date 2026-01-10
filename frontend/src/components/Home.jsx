0431720843b8857e0e517b5bffb715aa40b27171
Anirudha <anirudhakolekar65@gmail.com>
2026-01-10 10:16:58 +0530

    home update

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,47 +2,13 @@ import React, { useEffect, useState } from "react";
 import { FaTruck, FaBoxOpen, FaFlag, FaTshirt } from "react-icons/fa";
 import "aos/dist/aos.css";
 import AOS from "aos";
-
-const newArrivals = [
-  {
-    img: "bo4.jpeg",
-    name: "Stylern House Polo",
-    price: 1299,
-    originalPrice: 1799,
-  },
-  {
-    img: "bo5.jpeg",
-    name: "Gryffindor Polo",
-    price: 999,
-    originalPrice: 1499,
-  },
-  {
-    img: "bo6.jpeg",
-    name: "Triwizard T-shirt",
-    price: 2199,
-    originalPrice: 2999,
-  },
-  {
-    img: "bo7.jpeg",
-    name: "Hufflepuff  Knit Polo",
-    price: 1399,
-    originalPrice: 1999,
-  },
-  {
-    img: "bo8.jpeg",
-    name: "Horcruxes  T-shirt",
-    price: 699,
-    originalPrice: 999,
-  },
-  {
-    img: "bo9.jpeg",
-    name: "Premium Hoodie",
-    price: 1899,
-    originalPrice: 2499,
-  },
-];
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, FreeMode } from "swiper/modules";
 
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import "swiper/css";
+import "swiper/css/navigation";
+
 const Home = () => {
   useEffect(() => {
     AOS.init({
@@ -53,54 +19,62 @@ const Home = () => {
   const categories = [
     { img: "img1.jpg", title: "Mens" },
     { img: "img2.jpg", title: "Womens" },
-    // { img: "img3.jpg", title: "Kids" },
+    { img: "img3.jpg", title: "Kids" },
   ];
 
   const getDiscount = (price, originalPrice) => {
     return Math.round(((originalPrice - price) / originalPrice) * 100);
   };
 
-  const [startIndex, setStartIndex] = useState(0);
-  const [itemsPerPage, setItemsPerPage] = useState(4);
-
-  useEffect(() => {
-    const updateItems = () => {
-      if (window.innerWidth < 768) {
-        setItemsPerPage(2);
-      } else {
-        setItemsPerPage(4);
-      }
-    };
-
-    updateItems();
-    window.addEventListener("resize", updateItems);
-    return () => window.removeEventListener("resize", updateItems);
-  }, []);
-
-  const handleNext = () => {
-    if (startIndex + itemsPerPage < newArrivals.length) {
-      setStartIndex(startIndex + 1);
-    }
-  };
-
-  const handlePrev = () => {
-    if (startIndex > 0) {
-      setStartIndex(startIndex - 1);
-    }
-  };
-    const [imageLoaded, setImageLoaded] = useState(false);
-
+  const newArrivals = [
+    {
+      img: "bo4.jpeg",
+      name: "Stylern House Polo",
+      price: 1299,
+      originalPrice: 1799,
+    },
+    {
+      img: "bo5.jpeg",
+      name: "Gryffindor Polo",
+      price: 999,
+      originalPrice: 1499,
+    },
+    {
+      img: "bo6.jpeg",
+      name: "Triwizard T-shirt",
+      price: 2199,
+      originalPrice: 2999,
+    },
+    {
+      img: "bo7.jpeg",
+      name: "Hufflepuff  Knit Polo",
+      price: 1399,
+      originalPrice: 1999,
+    },
+    {
+      img: "bo8.jpeg",
+      name: "Horcruxes  T-shirt",
+      price: 699,
+      originalPrice: 999,
+    },
+    {
+      img: "bo9.jpeg",
+      name: "Premium Hoodie",
+      price: 1899,
+      originalPrice: 2499,
+    },
+  ];
 
   return (
     <div>
-      {/* <div className="w-full h-screen overflow-hidden">
-  <img
-    src="bo2.jpeg"
-    alt="hero"
-    className="w-full h-full object-cover object-[50%_20%]"
-  />
-</div> */}
-<div  className="relative w-full h-dvh overflow-hidden">
+      <div className="w-full h-screen overflow-hidden">
+        <img
+          src="bo2.jpeg"
+          alt="hero"
+          className="w-full h-full object-cover object-[50%_20%]"
+        />
+      </div>
+      {/* <div  className="relative w-full h-dvh overflow-hidden">
   <img
     src="bo2.jpeg"
     alt="hero"
@@ -124,21 +98,19 @@ const Home = () => {
   </div>}
 
  
-</div>
+</div> */}
 
-
-
-      <div className="categories grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-2 gap-1 px-1 py-1 ">
+      
+      <div className="categories grid grid-cols-3 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-2 gap-1 px-1 py-1 ">
         {categories.map((i, index) => (
           <div
             key={index}
             className="relative w-full  overflow-hidden rounded-md"
           >
-            {/* transition-all duration-300 hover:-translate-y-2 */}
             <img
               src={i.img}
               alt="catImg"
-              className="w-full h-full  object-cover "
+              className="w-full h-full object-cover "
             />
 
             <h3
@@ -151,6 +123,8 @@ const Home = () => {
         ))}
       </div>
 
+     
+
       <div data-aos="fade-up">
         <img
           src="bo3.jpeg"
@@ -159,7 +133,7 @@ const Home = () => {
         />
       </div>
 
-      <div  className="text-center py-8">
+      <div className="text-center py-8">
         <span className="text-xs uppercase tracking-[4px] text-gray-400">
           Fresh Collection
         </span>
@@ -173,27 +147,43 @@ const Home = () => {
         </p>
       </div>
 
-     <div className="relative px-4 mt-6 overflow-x-hidden">
-        <button
-          onClick={handlePrev}
-          disabled={startIndex === 0}
-          className="absolute left-3 top-1/2 -translate-y-1/2 bg-white border border-gray-300 rounded-full w-10 h-10 flex items-center justify-center shadow-md disabled:opacity-40"
-        >
+      <div className="relative px-4 mb-6">
+        <button className="men-prev absolute left-2 top-1/2 -translate-y-1/2 z-10 bg-white border rounded-full w-10 h-10 flex items-center justify-center shadow disabled:opacity-40 disabled:cursor-not-allowed disabled:pointer-events-none">
           <FaChevronLeft />
         </button>
 
-        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-4">
-          {newArrivals
-            .slice(startIndex, startIndex + itemsPerPage)
-            .map((item, index) => (
-              <div
-                key={index}
-                className="shadow-sm rounded-md overflow-hidden "
-              >
+        <button className="men-next absolute right-2 top-1/2 -translate-y-1/2 z-10 bg-white border rounded-full w-10 h-10 flex items-center justify-center shadow disabled:opacity-40 disabled:cursor-not-allowed disabled:pointer-events-none">
+          <FaChevronRight />
+        </button>
+
+        <Swiper
+          modules={[Navigation]}
+          navigation={{
+            prevEl: ".men-prev",
+            nextEl: ".men-next",
+            disabledClass: "swiper-button-disabled",
+          }}
+          spaceBetween={16}
+          speed={500}
+          slidesPerGroup={1}
+          grabCursor={true}
+          watchOverflow={true}
+          breakpoints={{
+            0: {
+              slidesPerView: 2,
+            },
+            768: {
+              slidesPerView: 4,
+            },
+          }}
+        >
+          {newArrivals.map((item, index) => (
+            <SwiperSlide key={index}>
+              <div className="shadow-sm rounded-md overflow-hidden">
                 <img
                   src={item.img}
-                  className="w-full h-60 sm:h-72 md:h-80 lg:h-112.5 object-cover"
-                  alt="product"
+                  alt={item.name}
+                  className="w-full h-60 sm:h-72 md:h-80 object-cover"
                 />
 
                 <div className="p-2">
@@ -210,17 +200,10 @@ const Home = () => {
                   </div>
                 </div>
               </div>
-            ))}
-        </div>
-
-        <button
-          onClick={handleNext}
-          disabled={startIndex + itemsPerPage >= newArrivals.length}
-          className="absolute right-3 top-1/2 -translate-y-1/2 bg-white border border-gray-300 rounded-full w-10 h-10 flex items-center justify-center shadow-md disabled:opacity-40"
-        >
-          <FaChevronRight />
-        </button>
-      </div> 
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
 
       <hr
         className="text-gray-300 px-2    pt-10 mx-3"
@@ -228,15 +211,14 @@ const Home = () => {
       />
 
       <div data-aos="fade-up bg-gray-300">
-          <img
-        src="bo10.jpeg"
-        alt="hero"
-        className="w-full h-64 sm:h-80 md:h-96 lg:h-170 object-cover "
-      />
+        <img
+          src="bo10.jpeg"
+          alt="hero"
+          className="w-full h-64 sm:h-80 md:h-96 lg:h-170 object-cover "
+        />
       </div>
-     
 
-      <div  className="mens md:px-7">
+      <div className="mens md:px-7">
         <div className="text-left py-8 px-4  ">
           <h2 className="mt-3 text-xl sm:text-2xl md:text-3xl font-serif font-semibold text-gray-800 tracking-tight">
             Shop Men
@@ -253,27 +235,43 @@ const Home = () => {
           </div>
         </div>
 
-       <div className="relative px-4 mt-1 mb-5 overflow-x-hidden">
-          <button
-            onClick={handlePrev}
-            disabled={startIndex === 0}
-            className="absolute left-3 top-1/2 -translate-y-1/2 bg-white border border-gray-300 rounded-full w-10 h-10 flex items-center justify-center shadow-md disabled:opacity-40"
-          >
+        <div className="relative px-4 mb-6">
+          <button className="men-prev absolute left-2 top-1/2 -translate-y-1/2 z-10 bg-white border rounded-full w-10 h-10 flex items-center justify-center shadow disabled:opacity-40 disabled:cursor-not-allowed disabled:pointer-events-none">
             <FaChevronLeft />
           </button>
 
-          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-4">
-            {newArrivals
-              .slice(startIndex, startIndex + itemsPerPage)
-              .map((item, index) => (
-                <div
-                  key={index}
-                  className="shadow-sm rounded-md overflow-hidden "
-                >
+          <button className="men-next absolute right-2 top-1/2 -translate-y-1/2 z-10 bg-white border rounded-full w-10 h-10 flex items-center justify-center shadow disabled:opacity-40 disabled:cursor-not-allowed disabled:pointer-events-none">
+            <FaChevronRight />
+          </button>
+
+          <Swiper
+            modules={[Navigation]}
+            navigation={{
+              prevEl: ".men-prev",
+              nextEl: ".men-next",
+              disabledClass: "swiper-button-disabled",
+            }}
+            spaceBetween={16}
+            speed={500}
+            slidesPerGroup={1}
+            grabCursor={true}
+            watchOverflow={true}
+            breakpoints={{
+              0: {
+                slidesPerView: 2,
+              },
+              768: {
+                slidesPerView: 4,
+              },
+            }}
+          >
+            {newArrivals.map((item, index) => (
+              <SwiperSlide key={index}>
+                <div className="shadow-sm rounded-md overflow-hidden">
                   <img
                     src={item.img}
-                    className="w-full h-60 sm:h-72 md:h-80 lg:h-112.5 object-cover"
-                    alt="product"
+                    alt={item.name}
+                    className="w-full h-60 sm:h-72 md:h-80 object-cover"
                   />
 
                   <div className="p-2">
@@ -290,25 +288,20 @@ const Home = () => {
                     </div>
                   </div>
                 </div>
-              ))}
-          </div>
-
-          <button
-            onClick={handleNext}
-            disabled={startIndex + itemsPerPage >= newArrivals.length}
-            className="absolute right-3 top-1/2 -translate-y-1/2 bg-white border border-gray-300 rounded-full w-10 h-10 flex items-center justify-center shadow-md disabled:opacity-40"
-          >
-            <FaChevronRight />
-          </button>
-        </div> 
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
 
         <hr
           className="text-gray-300 px-2 py-2   pt-3 mx-3"
           style={{ marginTop: "40px" }}
-          
         />
       </div>
-      <div data-aos="fade-up" className="max-w-4xl mx-auto px-4 py-6 pb-12 text-center" >
+      <div
+        data-aos="fade-up"
+        className="max-w-4xl mx-auto px-4 py-6 pb-12 text-center"
+      >
         <h2 className="text-3xl sm:text-4xl md:text-3xl font-extrabold text-gray-900">
           Clothing Store
         </h2>
@@ -325,7 +318,7 @@ const Home = () => {
         </p>
       </div>
 
-      <div className="max-w-7xl mx-auto px-4 pb-10 pt-10" >
+      <div className="max-w-7xl mx-auto px-4 pb-10 pt-10">
         <div className="grid grid-cols-2 md:grid-cols-4 gap-6 text-center">
           <div className="flex flex-col items-center">
             <div className="w-12 h-11 md:w-20 md:h-20 rounded-full border border-gray-300 flex items-center justify-center mb-3">
