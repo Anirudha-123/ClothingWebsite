ef0b52765080b03963211ba17369a8757ade25ac
Anirudha <anirudhakolekar65@gmail.com>
2026-01-12 16:51:45 +0530

    app update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import Header from "./components/Header.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/home/Home.jsx";
-import Contact from "./pages/contact/Contact.jsx";
+import Home from "./pages/Home/Home.jsx";
+import Contact from "./pages/Contact/Contact.jsx";
 import Footer from "./components/Footer.jsx";
-import Home1 from "./pages/home/Home1.jsx";
+import Home1 from "./pages/Home/Home1.jsx";
 import ScrollToTop from "./components/ScrollToTop.jsx";
 import ProductDetails from "./pages/ProductDetails.jsx";
-import Mens from "./pages/Mens/Mens.jsx";
 import Cart from "./pages/Cart/Cart.jsx";
+import Mens from "./pages/Mens/Mens.jsx";
+
+
 
 function App() {
   return (
